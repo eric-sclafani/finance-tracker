548b290adc92bcd0ce3c77404723718fdc41423b
eric-sclafani <eric.sclafani321@gmail.com>
2024-11-22 15:39:11 -0500

    added budget calculations

--- FinanceTracker/Pages/Index.cshtml.cs
+++ FinanceTracker/Pages/Index.cshtml.cs
@@ -20,10 +20,40 @@ public class IndexModel : PageModel
 		_context = context;
 	}
 
-	public async void OnGetAsync()
+	public void OnGet()
+	{
+		GetData();
+		UpdateBudget();
+	}
+
+
+	private async void GetData()
 	{
 		FixedExpenses = await _context.FixedExpenses.ToListAsync();
 		Purchases = await _context.Purchases.ToListAsync();
 		Budget = await _context.Budget.FirstAsync();
+	}
+
+	private int CalculateCashOut()
+	{
+		var expenses = from expense in FixedExpenses select expense.Amount;
+		var expensesSum = expenses.Sum();
+		return expensesSum;
+	}
+
+	private int CalculateDispoIncome()
+	{
+		return Budget.CashIn - (int)Budget.CashOut;
+	}
+
+	private async void UpdateBudget()
+	{
+		var entity = _context.Budget.FirstOrDefault(item => item.Id == 1);
+		if (entity != null)
+		{
+			entity.CashOut = CalculateCashOut();
+			entity.DisposableIncome = CalculateDispoIncome();
+			await _context.SaveChangesAsync();
+		}
 	}
 }
