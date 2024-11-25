4b1644e63be015e18dd3a359b8f45f34e6c1080f
eric-sclafani <eric.sclafani321@gmail.com>
2024-11-25 08:46:06 -0500

    updated display, finished for now

--- FinanceTracker/Models/Budget.cs
+++ FinanceTracker/Models/Budget.cs
@@ -7,5 +7,5 @@ public class Budget
 	public int Id { get; set; }
 	[Display(Name = "Cash In")] public int CashIn { get; set; }
 	[Display(Name = "Cash Out")] public double CashOut { get; set; }
-	[Display(Name = "Disposable Income")] public double DisposableIncome { get; set; }
+	[Display(Name = "Disp. Income")] public double DisposableIncome { get; set; }
 }
